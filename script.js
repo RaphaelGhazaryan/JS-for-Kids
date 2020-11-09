d36d80887c75eebaf99a2c15cb6a56a1b8bba820
RaphaelGhazaryan <raphael.ghaz@gmail.cm>
2020-11-09 14:57:12 +0100

    update

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 //JavaScript for Kids
+var secondsInAMinute = 60 ; 
+var minutesInAHour = 60 ;
+var secondsInAHour =  secondsInAMinute * minutesInAHour;
+console.log (secondsInAHour)
 
+var hoursInADay = 24 ; 
+var secondsInADay = hoursInADay * secondsInAHour;
+console.log (secondsInADay)
 
+var daysInAYear= 364;
+var secondsInAYear = secondsInADay * daysInAYear ;
+console.log (secondsInAYear)
 
+function AgeInSeconds(Age) {
+    AgeInSeconds= Age*secondsInAYear ; 
+    return console.log (`You are ${AgeInSeconds} seconds  old !!!`)
+    
+}
+AgeInSeconds (21);
