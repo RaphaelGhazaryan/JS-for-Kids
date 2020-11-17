6dc81383d55c39e15b4db2f4d1bca130b8e00be8
RaphaelGhazaryan <raphael.ghaz@gmail.cm>
2020-11-17 14:13:47 +0100

    firstChallenge

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
 //JavaScript for Kids
-var secondsInAMinute = 60 ; 
-var minutesInAHour = 60 ;
-var secondsInAHour =  secondsInAMinute * minutesInAHour;
-console.log (secondsInAHour)
 
-var hoursInADay = 24 ; 
-var secondsInADay = hoursInADay * secondsInAHour;
-console.log (secondsInADay)
 
-var daysInAYear= 364;
-var secondsInAYear = secondsInADay * daysInAYear ;
-console.log (secondsInAYear)
 
-function AgeInSeconds(Age) {
-    AgeInSeconds= Age*secondsInAYear ; 
-    return console.log (`You are ${AgeInSeconds} seconds  old !!!`)
-    
-}
-AgeInSeconds (21);
+
+//Random insult generator 
+Math.floor //rounds up number
+Math.random //gives random number from 0-1
+
+
+const firstWord = ['Scheiß', 'Dumm' , 'Ray' , 'Arsch'];
+const secondWord = ['klo', 'kotz', 'fett', 'schakal'];
+let thridWord = ['idiot', 'kopf' , 'schwein' , 'pimmel'];
+
+thridWord.push = ('lauch'); //Why is it not adding it to the Array?
+
+let firstWords = firstWord[Math.floor(Math.random() * 4)];
+let secondWords = secondWord[Math.floor(Math.random() * 4)];
+let thridWords = thridWord[Math.floor(Math.random() * thridWord.length)];
+
+
+const randomInsult = `Du bist ein ${firstWords}${secondWords}${thridWords} `;
+console.log(randomInsult)
