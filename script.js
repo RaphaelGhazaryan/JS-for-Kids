84adc067cd075d0dc24a399095cf453eac5fcdc2
RaphaelGhazaryan <raphael.ghaz@gmail.cm>
2020-11-26 13:10:39 +0100

    JS-for-Kids

--- script.js
+++ script.js
@@ -18,3 +18,43 @@ function AgeInSeconds(Age) {
     
 }
 AgeInSeconds(21);
+
+
+// Programming challenge 1 
+scores = {
+    raphael:0,
+    david:0,
+
+}
+scores.raphael += 2; 
+
+console.log(scores);
+
+
+
+var myCrazyObject = {
+    "name": "A ridiculous object",
+    "some array": [7, 9, { purpose: "confusion", number: 123 }, 3.3],
+    "random animal": "Banana Shark"
+   };
+
+   delete myCrazyObject["some array"].number ; //why is it not working ?
+
+console.log (myCrazyObject)
+
+
+// Programming challenge 2 
+
+var animals = ["Cat", "Fish","Lemur", "Komodo Dragon"];
+let awesomeanimals = []
+for (let rep = 0; rep < animals.length;rep ++) {
+    awesomeanimals.push ('Awesome ' + animals[rep]);
+}
+console.log(awesomeanimals)
+
+
+
+const alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+const alphabetRandom = alphabet[Math.floor(Math.random() * alphabet.length)];
+const randomAlphabet = `Dein Glücksbuchstabe is ${alphabetRandom}`;
+console.log(randomAlphabet);
