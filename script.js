6b574965648bf918739f5fade8b942f9a4ea85c7
RaphaelGhazaryan <raphael.ghaz@gmail.cm>
2020-12-03 09:17:01 +0100

    addedFiles

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+// Programming challenge 1
 
+// Add scores
+scores = {
+  raphael: 0,
+  david: 0,
+};
+scores.raphael += 2;
+console.log(scores);
+
+// delete number property
+var myCrazyObject = {
+  name: "A ridiculous object",
+  "some array": [7, 9, { purpose: "confusion", number: 123 }, 3.3],
+  "random animal": "Banana Shark",
+};
+
+delete myCrazyObject["some array"].number; //why is it not working ?
+
+console.log(myCrazyObject);
